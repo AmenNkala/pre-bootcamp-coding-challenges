7a1e8a9707df1f6d9824a0d3f267bd91b7516165
AmenNkala <amennkala@gmail.com>
2020-12-08 21:32:30 +0200

    changes in task9

--- task9.js
+++ task9.js
@@ -1,17 +1,9 @@
-var multiple3;
-var multiple5;
 var sum = 0;
 
-for(var i = 1; i < 1000; i++){
-    multiple3 = 3*i;
-    multiple5 = 5*i;
-    if(multiple3 < 1000){
-        sum += multiple3;
-    }
-
-    if(multiple5 < 1000){
-        sum += multiple5;
-    }
+for(var i = 0; i < 1000; i++){
+    if(i % 3 == 0 || i % 5 == 0){
+        sum += i;
+    }  
 }
 
 console.log(sum);
