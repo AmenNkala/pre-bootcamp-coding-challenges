d91b5321481401678ccacd6cbe68dbe519be112b
AmenNkala <amennkala@gmail.com>
2020-12-10 19:23:54 +0200

    changes in task4

--- task4.js
+++ task4.js
@@ -1,11 +1,8 @@
 function addTwoNumbers(x , y){  
-    return ((x === 3 || y === 3) && ((x + y) === 3));
+    return ((x === 3 || y === 3) && (String(x + y).includes(3)));
 }
 
 //test data
 console.log(addTwoNumbers(3 , 2)); 
-console.log(addTwoNumbers(4 , 3)); 
+console.log(addTwoNumbers(34 , 3)); 
 console.log(addTwoNumbers(3 , 0)); 
-console.log(addTwoNumbers(23, 0)); 
-console.log(addTwoNumbers(2 , 2)); 
-console.log(addTwoNumbers(7 , 6)); 
